382a49fd770fa0c459e19676c585d08590f8d94a
Lina <lterzieva7@gmail.com>
2022-08-08 15:15:58 +0300

    Comment section-edit and delete

--- client/src/components/detail/Detail.js
+++ client/src/components/detail/Detail.js
@@ -2,8 +2,12 @@ import { useEffect, useState, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import ReactPlayer from "react-player/youtube";
 
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar, faPenToSquare, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 import styles from './Detail.module.css';
 
@@ -24,10 +28,43 @@ const Detail = () => {
         description: "",
         rating: 0
     });
+
     const [hover, setHover] = useState(0);
+    const [editHover, setEditHover] = useState(0);
 
     const price = parseFloat(game.price).toFixed(2);
     const [validationMessage, setValidationMessage] = useState("");
+
+    const [editModal, setEditModal] = useState({
+        isOpen: false,
+        comment: {}
+    });
+
+    const handleClose = () => setEditModal({
+        isOpen: false,
+        comment: {}
+    });
+
+    function openFromParent(comment) {
+        setEditModal({
+            isOpen: true,
+            comment: comment
+        });
+
+        setEditHover(0);
+
+    }
+
+    const handleEditChange = (e) => {
+        const value = e.target.value;
+        setEditModal({
+            ...editModal,
+            comment: {
+                ...editModal.comment,
+                [e.target.name]: value
+            }
+        });
+    }
 
     useEffect(() => {
         fetch(`http://localhost:3030/data/games/${gameId}`)
@@ -91,6 +128,65 @@ const Detail = () => {
 
         setHover(0);
 
+    }
+
+
+    const submitEditHandler = (e) => {
+        e.preventDefault();
+
+        // if (editModal.comment == "") {
+        //     return setValidationMessage("Please enter a comment");
+        // }
+
+        fetch(`http://localhost:3030/data/comments/${editModal.comment._id}`, {
+            method: 'PUT',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                'X-Authorization': `${auth.accessToken}`
+            },
+            body: JSON.stringify({
+                _gameId: editModal.comment._gameId,
+                description: editModal.comment.description,
+                rating: editModal.comment.rating
+            }),
+        })
+        .then((response) => {
+            if (response.status == 200) {
+                fetch(`http://localhost:3030/data/comments?where=_gameId%3D%22${gameId}%22&load=author%3D_ownerId%3Ausers`)
+                    .then(response => response.json())
+                    .then(data => {
+                        setAllComments(data);
+                    });
+            }
+            //403 forbidden error
+        })
+
+        setEditModal({
+            isOpen: false,
+            comment: {}
+        })
+    }
+
+    const handleDelete = (commentId) => {
+        fetch(`http://localhost:3030/data/comments/${commentId}`, {
+            method: 'DELETE',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                'X-Authorization': `${auth.accessToken}`
+            },
+        })
+            .then((response) => {
+                if (response.status == 200) {
+                    fetch(`http://localhost:3030/data/comments?where=_gameId%3D%22${gameId}%22&load=author%3D_ownerId%3Ausers`)
+                        .then(response => response.json())
+                        .then(data => {
+                            setAllComments(data);
+                        });
+                }
+                //403 forbidden error
+            })
     }
 
     return (
@@ -181,26 +277,90 @@ const Detail = () => {
                                 All Comments
                             </div>
                             {allComments && allComments.map((comment) => {
-                                return (<div className={styles.commentCard} key={comment._id}>
-                                    <div className="row d-flex">
-
-                                        <div className="d-flex flex-column">
-                                            <h3 className={`${styles.commentCardAuthor} mt-2 mb-0`}>{comment.author.username}</h3>
-
+                                return (
+                                    <div className={styles.commentCard} key={comment._id}>
+                                        {comment._ownerId == auth.id &&
+                                            <div className={styles.commentCardActions}>
+                                                <button onClick={() => openFromParent(comment)}>
+                                                    <FontAwesomeIcon icon={faPenToSquare} className={styles.cardActionIcon} />
+                                                </button>
+                                                <button onClick={() => handleDelete(comment._id)}>
+                                                    <FontAwesomeIcon icon={faTrashCan} className={styles.cardActionIcon} />
+                                                </button>
+                                            </div>
+                                        }
+                                        <div className="row d-flex">
+                                            <div className="d-flex flex-column">
+                                                <h3 className={`${styles.commentCardAuthor} mt-2 mb-0`}>{comment.author.username}</h3>
+                                            </div>
+                                            <div className="ml-auto">
+                                                <p className="text-muted pt-5 pt-sm-3">
+                                                    {generateStars(comment.rating)}
+                                                </p>
+                                            </div>
                                         </div>
-                                        <div className="ml-auto">
-                                            <p className="text-muted pt-5 pt-sm-3">
-                                                {generateStars(comment.rating)}
-                                            </p>
+                                        <div className="row text-left">
+                                            <h4 className="text-primary mt-3">{comment.description}</h4>
                                         </div>
                                     </div>
-                                    <div className="row text-left">
-                                        <h4 className="text-primary mt-3">{comment.description}</h4>
-                                    </div>
-
-                                </div>)
+                                )
                             })}
 
+                            <Modal show={editModal.isOpen} onHide={handleClose}>
+                                <Modal.Header closeButton>
+                                    <Modal.Title>Edit your comment</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>
+                                    <Form onSubmit={(e) => submitEditHandler(e)}>
+                                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                            <div className={styles.ratingStars}>
+                                                {[...Array(5)].map((star, index) => {
+                                                    index += 1;
+
+                                                    return (
+                                                        <button
+                                                            type="button"
+                                                            key={index}
+                                                            className={index <= (editHover || editModal.comment.rating) ? `${styles.star}` : `${styles.off}`}
+                                                            onClick={() => setEditModal((state) => {
+                                                                return ({
+                                                                    ...state,
+                                                                    comment: {
+                                                                        ...editModal.comment,
+                                                                        rating: index
+                                                                    }
+                                                                })
+                                                            })}
+                                                            onMouseEnter={() => setEditHover(index)}
+                                                            onMouseLeave={() => setEditHover(editModal.comment.rating)}
+                                                        >
+                                                            <FontAwesomeIcon icon={faStar} className={styles.ratingStar} />
+                                                        </button>
+                                                    );
+                                                })}
+                                            </div>
+                                        </Form.Group>
+                                        <Form.Group
+                                            className="mb-3"
+                                            controlId="exampleForm.ControlTextarea1"
+                                        >
+                                            <Form.Control as="textarea" rows={3} value={editModal.comment.description} onChange={handleEditChange} name="description" />
+                                        </Form.Group>
+                                        {/* <Button variant="primary" type="submit" value="Save Changes">
+                                            Save Changes
+                                        </Button> */}
+                                        <input type="submit" value="Submit" />
+                                    </Form>
+                                </Modal.Body>
+                                {/* <Modal.Footer>
+                                    <Button variant="secondary" onClick={handleClose}>
+                                        Discard Changes
+                                    </Button>
+                                    <Button variant="primary" onClick={handleClose} type="submit">
+                                        Save Changes
+                                    </Button>
+                                </Modal.Footer> */}
+                            </Modal>
 
                         </div>
 
