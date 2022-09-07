68f9ab933e215f658cfb15740bbdf3b3e93ee571
Lina <lterzieva7@gmail.com>
2022-09-07 17:40:36 +0300

    fix: empty cart - display a message.

--- client/src/components/cart/Cart.js
+++ client/src/components/cart/Cart.js
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import CartContext from '../../lib/CartContext';
 
 import Card from 'react-bootstrap/Card';
@@ -11,30 +12,50 @@ const Cart = () => {
 
     const { cart } = useContext(CartContext);
 
-    const totalPrice = cart.reduce((a, game) => a += game.price*game.quantity, 0);
+    console.log(cart);
+
+    const totalPrice = cart.reduce((a, game) => a += game.price * game.quantity, 0);
 
     return (
         <Card className={styles.cart}>
             <Card.Header className={`${styles.cartHeader} px-4 py-3`} as="h5">Your shopping cart</Card.Header>
             <Card.Body className={styles.cardBody}>
-                <div className={styles.cartItems}>
-                    {cart.map((game) => {
-                        return <CartItem key={game.gameId} game={game} />
-                    })}
-                </div>
-                <div className={`${styles.cartTotal} pt-3 pb-2 px-4`}>
-                    <div className={styles.cartTotalPrice}>
-                        {totalPrice.toFixed(2)}€
+                {cart.length
+                    ? <>
+                        <div className={styles.cartItems}>
+                            {cart.map((game) => {
+                                return <CartItem key={game.gameId} game={game} />
+                            })}
+                        </div>
+                        <div className={`${styles.cartTotal} pt-3 pb-2 px-4`}>
+                            <div className={styles.cartTotalPrice}>
+                                {totalPrice.toFixed(2)}€
+                            </div>
+                            <div className={styles.cartTotalTitle}>
+                                Total
+                            </div>
+                        </div>
+                        <div className={`${styles.cartCheckout} py-3 px-2`}>
+                            <button className={styles.cartCheckoutBtn}>
+                                Go To Checkout
+                            </button>
+                        </div>
+                    </>
+                    : <div className={styles.cartEmptyMsg}>
+                        <div className={styles.cartEmptyMsgTitle}>
+                            Your cart is currently empty.
+                        </div>
+                        <div className={styles.cartEmptyMsgDscr}>
+                            Before proceed to checkout you must add some products to your shopping cart.
+                        </div>
+                        <div className={styles.cartShopBox}>
+                            <Link to='/catalog' className={styles.cartShopLink}>
+                                Shop now
+                            </Link>
+                        </div>
                     </div>
-                    <div className={styles.cartTotalTitle}>
-                        Total
-                    </div>
-                </div>
-                <div className={`${styles.cartCheckout} py-3 px-2`}>
-                    <button className={styles.cartCheckoutBtn}>
-                        Go To Checkout
-                    </button>
-                </div>
+                }
+
             </Card.Body>
         </Card>
     );
